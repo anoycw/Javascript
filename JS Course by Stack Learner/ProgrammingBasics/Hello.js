1724a1bf2acd9d15a0963a12e57060fc8815bbd5
anoy <anoy@eatlbd.com>
2023-02-26 17:33:33 +0600

    js initial

---
+++ JS Course by Stack Learner/ProgrammingBasics/Hello.js
@@ -0,0 +1,7 @@
+//Q: What is Programming ?
+//A: Programming is a set of instruction which our computer can read,understand and produce output.
+
+console.log("Hello World...!!");
+
+console.log("I Love Bangladesh :" + 10)  // String + Number = String     [This is called string concat]
+console.log("9" + 9); // Ans is 99 because String + anything = String
---
+++ JS Course by Stack Learner/ProgrammingBasics/variables.js
@@ -0,0 +1,55 @@
+// Variable is a Container which can Store Data.
+// Variable declration
+
+var name = "Anoy Chowdhury"  // var stands for variable
+var age = 27;
+
+console.log(name);
+console.log(age);
+
+
+// Variable should declare first then  use.
+console.log(name);
+console.log(age);
+
+var name = "Anoy Chowdhury"  // var stands for variable
+var age = 27;
+
+// This case results Undefined
+
+// In the variable, we can store any kind of data.
+
+
+// Variable name can be anything except some reserved keywords
+// abstract	arguments	await*	boolean
+// break	byte	case	catch
+// char	class*	const	continue
+// debugger	default	delete	do
+// double	else	enum*	eval
+// export*	extends*	false	final
+// finally	float	for	function
+// goto	if	implements	import*
+// in	instanceof	int	interface
+// let*	long	native	new
+// null	package	private	protected
+// public	return	short	static
+// super*	switch	synchronized	this
+// throw	throws	transient	true
+// try	typeof	var	void
+// volatile	while	with	yield
+
+
+
+// const false = "something" ;  // Error
+// var throw; // Error
+// var try; // Error
+
+
+// Naming Convention - Js follows camelCase Naming convention.
+
+var mathNumber = 10;
+
+
+
+
+
